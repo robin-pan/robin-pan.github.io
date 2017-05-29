03c18a7bf42b975ace486f0cafa4ed15e495c5d4
robin-pan <pan923pan@gmail.com>
2017-05-29 19:01:52 -0400

    Added dropdown menu

---
+++ js/dropdown.js
@@ -0,0 +1,12 @@
+$('.hamburger').on('click', function(e) {
+  // Prevent link from jumping to the top of the page
+  e.preventDefault();
+  // If menu is already showing, slide it up. Otherwise, slide it down.
+  $('.menu').toggleClass('slide-down');
+});
+
+$('.menuitem').on('click', function(e) {
+  e.preventDefault();
+  
+  $('.menu').toggleClass('slide-down');
+});
