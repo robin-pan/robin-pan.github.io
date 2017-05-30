6069e52f171d92acac8752beaefbc0d882c64113
robin-pan <pan923pan@gmail.com>
2017-05-29 23:40:14 -0400

    Added animation for menu icon on mobile

--- js/dropdown.js
+++ js/dropdown.js
@@ -3,14 +3,17 @@ $('.hamburger').on('click', function(e) {
   e.preventDefault();
   // If menu is already showing, slide it up. Otherwise, slide it down.
   $('.menu').toggleClass('slide-down');
+  $('.hamburger').toggleClass('change');
 });
 
 $('.menuitem').on('click', function(e) {
   $('.menu').toggleClass('slide-down');
+  $('.hamburger').toggleClass('change');
 });
 
 $(window).resize(function() {
 	if ($(window).width() > 605) {
 		$('.menu').removeClass('slide-down');
+		$('.hamburger').removeClass('change');
 	}
 });
